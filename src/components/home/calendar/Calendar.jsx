e25fcb2e8f98b89105c0ff98cc9a66a37e768898
DevFrank1 <fbaylarzade11@gmail.com>
2023-01-02 19:32:12 +0400

    calendar added 1.0

--- src/components/home/calendar/Calendar.jsx
+++ src/components/home/calendar/Calendar.jsx
@@ -1,9 +1,27 @@
+import { Box } from '@mui/material';
 import React from 'react';
 import './calendar.css';
 
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
 const Calendar = () => {
   return (
-    <div>Calendar</div>
+    <Box sx={{ display: 'block', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%', padding: '1rem' }}>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView='dayGridMonth'
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        }}
+        height='100%'
+        contentHeight='100%'
+      />
+    </Box>
   )
 }
 
