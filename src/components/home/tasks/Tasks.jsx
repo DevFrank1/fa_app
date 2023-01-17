3e9b55e28ac38e06c4f7e2b00e7bd693dfed7f18
DevFrank1 <fbaylarzade11@gmail.com>
2023-01-17 21:27:44 +0400

    tasks initialization

---
+++ src/components/home/tasks/Tasks.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import './tasks.css';
+
+const Tasks = () => {
+  return (
+    <Box>
+        Tasks
+    </Box>
+  )
+}
+
+export default Tasks
